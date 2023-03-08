cdc07bb35c301f8acb01cbc0036d348228d286aa
Arsen Aramyan <77010946+araramya@users.noreply.github.com>
2023-03-08 16:12:02 +0000

    saved

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -13,6 +13,55 @@ Channel::~Channel()
     //default destructor
 }
  
+
+//getters
+std::string Channel::GetName() const
+{
+    return m_name;
+}
+
+std::string Channel::GetTopic() const
+{
+    return m_topic;
+}
+
+std::string Channel::GetPassword() const
+{
+    return m_password;    
+}
+
+int Channel::GetNumberOfClients() const
+{ 
+    // get_number
+}
+
+int Channel::GetMaxClients() const
+{
+    return m_max_clients;
+}
+
+//setters
+void Channel::SetName(const std::string &name)
+{
+    m_name = name;
+}
+
+void Channel::SetTopic(const std::string &topic)
+{
+    m_topic = topic;
+}
+
+void Channel::SetPassword(const std::string &key)
+{
+    m_password = key;
+}
+
+void Channel::SetMaxClients(const int max_clients)
+{
+    m_max_clients = max_clients;
+}
+
+// working with clinets
 void Channel::AddClient(Client* client)
 {
         //Adding client
@@ -31,28 +80,18 @@ void Channel::RemoveClient(Client* client)
     } 
 }
 
-void Channel::BroadcastMessage()
+void Channel::BroadcastMessage(const std::string& message)
 {
     //not sure how this should be work
     //brodcast message that will be shown all users.
 }
 
-std::string Channel::GetName() const
+void Channel::BrodcastMessage(const std::string& message, Client *client )
 {
-    return m_name;
+    //brodcast message that will be shown for all users.
 }
 
-std::string Channel::GetTopic() const
+void Channel::Kick(Client* admin, Client *client, std::string s_reason)
 {
-    return m_topic;
-}
-
-void Channel::SetName(const std::string &name)
-{
-    m_name = name;
-}
-
-void Channel::SetTopic(const std::string &topic)
-{
-    m_topic = topic;
+    //kick from channel
 }
